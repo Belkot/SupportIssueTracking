cf6287177cadec261147f92539570f7517694d27
Oleh Belkot <o.belkot@gmail.com>
2015-04-22 14:43:25 +0300

    Add Department.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422112624) do
+ActiveRecord::Schema.define(version: 20150422114257) do
+
+  create_table "departments", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tickets", force: true do |t|
     t.string   "name"
