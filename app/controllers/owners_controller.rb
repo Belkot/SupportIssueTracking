3d97c9f3e52d5065f31330a58b1166b1c7fb5d65
Oleh Belkot <o.belkot@gmail.com>
2015-04-27 13:13:44 +0300

    Add authenticate to departments, owners, status_types.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,5 @@
 class OwnersController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @owner = Owner.new
--- app/controllers/status_types_controller.rb
+++ app/controllers/status_types_controller.rb
@@ -1,4 +1,5 @@
 class StatusTypesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_status_type, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
