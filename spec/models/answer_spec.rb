7e541909449c8af07d303980068b33ee5ab29259
Oleh Belkot <o.belkot@gmail.com>
2015-04-29 13:32:01 +0300

    Add answers spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a body and ticket_id" do
+    answer = Answer.new(body: 'Test answer')
+    expect(answer).to be_valid
+  end
+
+  it "is invalid without a body" do
+    answer = Answer.new
+    answer.valid?
+    expect(answer.errors[:body]).to include("can't be blank")
+  end
+
+  it "is valid with a duplicate body" do
+    Answer.create(body: 'Test 2 answer')
+    answer = Answer.new(body: 'Test 2 answer')
+    answer.valid?
+    expect(answer).to be_valid
+  end
+
+  it "is invalid with a short body" do
+    answer = Answer.new(body: 'Abcdefghi')
+    answer.valid?
+    expect(answer.errors[:body]).to include("is too short (minimum is 10 characters)")
+  end
+
+  it "is invalid with a long body" do
+    long_name = 'a' * 4001
+    answer = Answer.new(body: long_name)
+    answer.valid?
+    expect(answer.errors[:body]).to include("is too long (maximum is 4000 characters)")
+  end
 end
