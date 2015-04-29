a6a9469250f02b1de1a864a859115aa1693e4a50
Oleh Belkot <o.belkot@gmail.com>
2015-04-29 13:03:02 +0300

    Add status type spec

--- app/models/status_type.rb
+++ app/models/status_type.rb
@@ -1,4 +1,6 @@
 class StatusType < ActiveRecord::Base
   has_many :statuses
   has_many :tickets, through: :statuses
+
+  validates :name, presence: true,  uniqueness: true, length: { in: 2..255 }
 end
--- spec/models/status_type_spec.rb
+++ spec/models/status_type_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe StatusType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name" do
+    status_type = StatusType.new(name: 'Test status type')
+    expect(status_type).to be_valid
+  end
+
+  it "is invalid without a name" do
+    status_type = StatusType.new
+    status_type.valid?
+    expect(status_type.errors[:name]).to include("can't be blank")
+  end
+
+  it "invalid with a duplicate name" do
+    StatusType.create(name: 'Test 2 status type')
+    status_type = StatusType.new(name: 'Test 2 status type')
+    status_type.valid?
+    expect(status_type.errors[:name]).to include('has already been taken')
+  end
+
+  it "invalid with a short name" do
+    status_type = StatusType.new(name: 'A')
+    status_type.valid?
+    expect(status_type.errors[:name]).to include("is too short (minimum is 2 characters)")
+  end
+
+  it "invalid with a long name" do
+    long_name = 'a' * 256
+    status_type = StatusType.new(name: long_name)
+    status_type.valid?
+    expect(status_type.errors[:name]).to include("is too long (maximum is 255 characters)")
+  end
 end
